aed7c93d0d1d09f09d2032d5df4710a6263492b1
mnslbs <tjs5007@ajou.ac.kr>
2023-06-20 13:21:31 +0900

    Unit 클래스 생성

---
+++ Pinokio.MUM-T/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinokio.MUM_T
+{
+    internal class Unit
+    {
+        //기종, 타격가능(지/공), 작전 범위, 연료, 무장, 라우팅, 가능 임무, 현재 위치
+        private string _type;
+        private List<string> _attackRange;
+        private string _operationRange;
+        private float _fuel;
+        private float _fuelSpeed;
+        private float _fuelRate;
+        private Dictionary<string, Dictionary<string,int>> _arm
+    }
+}
