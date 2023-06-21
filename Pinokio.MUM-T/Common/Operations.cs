ecd65add2f3b2a90ec39672a5fa36eb9f9797dc8
Simon Anderson <bbubgsang94@ajou.ac.kr>
2023-06-21 16:11:01 +0900

    명세화

--- Pinokio.MUM-T/Common/Operations.cs
+++ Pinokio.MUM-T/Common/Operations.cs
@@ -10,9 +10,27 @@ namespace Pinokio.MUM_T.Common
     {
     }
 
+    internal class Mission
+    {
+        
+    }
+
+    internal class Task
+    {
+        public int Id = -1;
+        public List<Unit> TeamUnits = new List<Unit>();     // 아군 유닛
+        public List<Unit> TargetUnits = new List<Unit>();   // 목표 유닛
+        public DateTime ElapsedTime;                        // 흘러간 시간
+        public DateTime StartTime;                          // 시작 시간
+        public DateTime DueDate;                            // 제한 시간
+        public int Priority;                                // 우선순위
+        public float Threat;                                // 태스크 자체 위협도
+        public Tuple<float, float, float>? TargetLocation;  // 목표 위치
+    }
+
     internal class Unit
     {
-        public int Id = 0;
+        public int Id = -1;
         public int Kind = 0;    // 대상: 001(지상) 010(해상) 100(공중)
         public int Mission; // 미션 종류 (2진): 000001(정찰) 000010(타격) 000100(섬멸) 001000(보호) 010000(방어) 100000(억제)
         public List<Weapon> Weapons = new();    // 무장
@@ -26,7 +44,7 @@ namespace Pinokio.MUM_T.Common
 
     internal class Weapon
     {
-        public int Id;
+        public int Id = -1;
         public string? Name;
         public string? Description;
         public int? Target; // 공격가능한 대상 (2진): 001(지상) 010(해상) 100(공중) 101(지상+공중) 111(지상+해상+공중)
