de738c2082875adf7cf999f2c8dba58536770e52
Simon Anderson <bbubgsang94@ajou.ac.kr>
2023-06-22 20:07:22 +0900

    gg

--- Pinokio.MUM-T/Program.cs
+++ Pinokio.MUM-T/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Pinokio.Core;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+
+LogHandler.LogInfoHandle += Console.WriteLine;
+LogHandler.LogErrorHandle += Console.WriteLine;
 Console.WriteLine("Hello, World!");
+
+
+static bool LoadLexicon()
+{
+    LogHandler.AddLog(LogLevel.Info, "Start to Load Lexicon");
+
+    if (GetZMQPort(out int zmqPort))
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        document = new RLDocument(zmqPort);
+        document.GenerateMapModel("SMT");
+        document.LoadMapData(printDot);
+        LogHandler.AddLog(LogLevel.Info, "");
+        sw.Stop();
+
+        LogHandler.AddLog(LogLevel.Info, $"Finish to Load Models({sw.ElapsedMilliseconds})");
+        return true;
+    }
+
+    return false;
+}
